5639f6be15abe755b9ef91a8006d637593abe0d1
mrmobilos <90411669+Swey999@users.noreply.github.com>
2024-11-06 10:04:28 +0100

    Test Project Added

---
+++ TicketJam.Test/CreateOrder.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TicketJam.Test.CreateOrder.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
