a37502a9aac5e389c266dfeac3324252cdfee4ea
mrmobilos <90411669+Swey999@users.noreply.github.com>
2024-11-07 08:42:24 +0100

    Order create project,

---
+++ TicketJam.WebAPI/Controllers/OrderControllerAPI.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketJam.DAL.DAO;
+using TicketJam.DAL.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TicketJam.WebAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderControllerAPI : ControllerBase
+    {
+        public IDao<Order> _orderDAO;
+        private IConfiguration _configuration;
+        public OrderControllerAPI(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            string connectionString = _configuration.GetConnectionString("DBConnectionString");
+            _orderDAO = new OrderDao(connectionString);
+
+        }
+
+        // GET: api/<OrderControllerAPI>
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetAll()
+        {
+            return Ok(_orderDAO.Read());
+        }
+
+        // GET api/<OrderControllerAPI>/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetById(int id)
+        {
+            Order order = _orderDAO.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(order);
+            }
+        }
+
+        // POST api/<OrderControllerAPI>
+        [HttpPost]
+        public ActionResult<Order> Post(Order Order)
+        {
+            _orderDAO.Create(Order);
+            return Ok(Order);
+        }
+
+        // PUT api/<OrderControllerAPI>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(Order order)
+        {
+            _orderDAO.Update(order);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+
+        // DELETE api/<OrderControllerAPI>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (!_orderDAO.Delete(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+    }
+}
---
+++ TicketJam.Website/APIClient/DTO/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace TicketJam.Website.APIClient.DTO
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public int EventNo { get; set; }
+        //public string Description { get; set; }
+        public int TotalAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Venue Venue { get; set; }
+       // public Organizer Organizer { get; set; }
+    }
+}
