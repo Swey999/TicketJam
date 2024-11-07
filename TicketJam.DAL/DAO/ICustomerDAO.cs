2fe71c1462bc08df2a767f89d91696ee343eb404
ThomasUCN1996 <10503094@ucn.dk>
2024-11-07 09:04:42 +0100

    Customer er træls

---
+++ TicketJam.DAL/DAO/ICustomerDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketJam.DAL.DAO
+{
+    public interface ICustomerDAO<Customer>
+
+    {
+        int Insert(Customer customer);
+        IEnumerable<Customer> GetAll();
+        Customer Get(int id);
+        bool Update(Customer customer);
+        bool Delete(Customer customer);
+
+    }
+}
---
+++ TicketJam.Website/DTO/AddressDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketJam.Website.DTO
+{
+    public class AddressDTO
+    {
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string HouseNo { get; set; }
+    }
+}
