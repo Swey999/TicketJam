5f823a58fda2e932b05d029b973c2ac27cec4567
mrmobilos <90411669+Swey999@users.noreply.github.com>
2024-11-11 11:10:34 +0100

    CART ADDED, EVENT CHANGED

---
+++ TicketJam.Website/APIClient/TicketAPIConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using TicketJam.Website.APIClient.DTO;
+
+namespace TicketJam.Website.APIClient
+{
+    public class TicketAPIConsumer : Controller
+    {
+        private string BaseURI;
+        private RestClient restClient;
+        public TicketAPIConsumer(string baseURI)
+        {
+            restClient = new RestClient(baseURI);
+            BaseURI = baseURI;
+        }
+        public Ticket GetById(int id)
+        {
+            var client = new RestClient($"{BaseURI}/{id}");
+
+            var response = client.ExecuteGet<Ticket>(new RestRequest());
+
+            if (!response.IsSuccessful || response == null)
+            {
+                throw new Exception("Unable to call that thing that thing");
+            }
+
+            return response.Data;
+        }
+    }
+}
---
+++ TicketJam.Website/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TicketJam.Website.APIClient;
+using TicketJam.Website.APIClient.DTO;
+
+namespace TicketJam.Website.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: CartController
+        TicketAPIConsumer _ticketAPIConsumer = new TicketAPIConsumer("https://localhost:7280/api/TicketControllerAPI");
+        public ActionResult Index()
+        {
+            return View(GetCartFromCookie());
+        }
+
+        public ActionResult Add(int id, int quantity)
+        {
+            OrderLine orderline = new OrderLine();
+            Order order = GetCartFromCookie();
+            orderline.Ticket = _ticketAPIConsumer.GetById(id);
+            orderline.Quantity = quantity;
+            order.OrderLines.Add(orderline);
+           // order.ChangeQuantity(new ProductQuantity(product, quantity));
+            SaveCartToCookie(order);
+            return View("Index", order);
+        }
+
+        private Order GetCartFromCookie()
+        {
+            Request.Cookies.TryGetValue("Order", out string? cookie);
+            if (cookie == null) { return new Order(); }
+            return JsonSerializer.Deserialize<Order>(cookie) ?? new Order();
+        }
+
+        private void SaveCartToCookie(Order order)
+        {
+            var cookieOptions = new CookieOptions();
+            cookieOptions.Expires = DateTime.Now.AddDays(7);
+            cookieOptions.Path = "/";
+            Response.Cookies.Append("Order", JsonSerializer.Serialize(order), cookieOptions);
+        }
+
+        public ActionResult EmptyCart()
+        {
+            Order order = GetCartFromCookie();
+            order.OrderLines = new List<OrderLine>(); //Check om det virker nyt shit
+            SaveCartToCookie(order);
+            return View("Index", order);
+        }
+    }
+}
