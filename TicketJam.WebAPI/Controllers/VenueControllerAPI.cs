5bf5ddc70245cb37d3402ea580e044547de8b2bd
alexbusk1 <10503115@ucn.dk>
2024-11-13 13:29:56 +0100

    Created new venueControllerAPI

--- TicketJam.WebAPI/Controllers/VenueControllerAPI.cs
+++ TicketJam.WebAPI/Controllers/VenueControllerAPI.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TicketJam.DAL.DAO;
 using TicketJam.DAL.Model;
+using TicketJam.WebAPI.DTOs;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,45 @@ namespace TicketJam.WebAPI.Controllers
     [ApiController]
     public class VenueControllerAPI : ControllerBase
     {
-        
+        public IDAO<Venue> _venueDAO;
+        // GET: api/<VenueControllerAPI>
+        [HttpGet]
+        public ActionResult<IEnumerable<Venue>> Get()
+        {
+            return Ok(_venueDAO.Read());
+        }
+
+        // GET api/<VenueControllerAPI>/5
+        [HttpGet("{id}")]
+        public ActionResult<Venue> GetById(int id)
+        {
+            Venue venue = _venueDAO.GetById(id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(venue);
+            }
+        }
+
+        // POST api/<VenueControllerAPI>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<VenueControllerAPI>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<VenueControllerAPI>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
